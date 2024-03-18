fcb9a08ab8b2bdebc9232a88c11b559e0b107d93
adisa <abubakradisa@gmail.com>
2024-03-18 10:48:42 +0100

    Removed duplicate button

--- src/components/CardTwo.vue
+++ src/components/CardTwo.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="card bg-white mx-4 p-4 rounded-xl relative -top-28 -mt-2 md:mx-0 md:-top-16 md:-mt-0 space-y-8">     
-<div>  
+<div class="card mx-2 py-6 px-2 bg-white rounded-xl relative -top-28 -mt-2 md:mx-0 md:-top-16 md:-mt-0 space-y-8">     
+<div>
 <h1 class="text-3xl text-marineblue font-bold">Select your plan</h1>
-<p class="text-lg text-coolGray">You have the option of monthly or yearly billing.</p>
+<p class="text-lg text-coolgray">You have the option of monthly or yearly billing.</p>
 </div>
-<div class="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
+<div class="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">    
     <div class="flex md:w-40 md:flex-col items-center md:items-start border-2 space-x-6 md:space-x-0 p-4 rounded-xl md:space-y-10">                 
     <div>
         <img src="../images/icon-arcade.svg" alt="arcade" class="w-14">
@@ -33,7 +33,9 @@
     </div>
     </div>    
 </div>
-<button class="float-left hover:scale-110 hover:transition-all hover:duration-500 bg-lightgray text-black border-0 p-3 rounded-lg hidden md:block">Go Back</button>
-<button class="float-right hover:scale-110 hover:transition-all hover:duration-500 bg-marineblue text-white border-0 p-3 rounded-lg hidden md:block">Next Step</button>
+<div class="flex justify-between relative top-32">
+    <button class="hover:scale-110 hover:transition-all hover:duration-500  text-lightgray border-0 p-3 rounded-lg md:block">Go Back</button>
+    <button class="hover:scale-110 hover:transition-all hover:duration-500 bg-marineblue text-white border-0 p-3 rounded-lg  md:block">Next Step</button>            
+    </div>          
 </div>
 </template>
--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -8,12 +8,8 @@ const previousRoute = () => {
 }
 </script>
 <template>
- <main class="md:flex md:justify-around md:items-center md:mx-auto md:mt-8 md:rounded-xl md:py-8 md:bg-white md:w-3/4">
+ <main class="md:flex md:justify-around md:items-center md:mx-auto md:mt-8 md:rounded-xl md:py-8 md:bg-white md:w-3/4 relative">
     <Nav/>
     <CardTwo/>
-    <div class="flex justify-between p-4"> 
-    <button class="text-lightGray bg-lightgray border-0 w-20 h-14 rounded-lg -mt-20 md:hidden outline-none" @click="previousRoute">Go Back</button>    
-    <button class=" bg-marineblue text-white border-0 w-20 h-14  rounded-lg -mt-20 md:hidden outline-none">Next Step</button>    
-    </div>    
  </main>    
 </template>
